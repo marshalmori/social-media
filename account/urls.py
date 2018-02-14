4e2b81dfc88dce5f01c0561844e1e4355eeb01fb
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-02-14 19:00:25 -0200

    Criação do ambiente para mudança de senha

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     url(r'^$', views.dashboard, name='dashboard'),
+
+    # Abaixo temos as urls para mudança de senha
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
 ]
